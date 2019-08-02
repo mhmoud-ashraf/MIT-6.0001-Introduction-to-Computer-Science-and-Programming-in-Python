9a5b8fed1bd29d922db6e2f740daeb6fb37d2413
Mahmoud Ashraf Mohamed <m.ashrafmohamed@hotmail.com>
2019-08-02 12:09:11 +0200

    ps0
    Introduction to the programming environment Spyder from the Anaconda Distribution of Python, and to programming in Python, as well as to the MIT 6.0001 general problem set structure.

---
+++ ps0-simple_python_program/ps0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 30 22:37:15 2019
+
+@author: Mahmoud Ashraf
+"""
+
+import numpy
+x = int(input("Enter a number X:   "))
+y = int(input("Enter an other number Y:   "))
+print("X**Y =", x**y , "\n"+
+      "log(x) =" , numpy.log2(x))
